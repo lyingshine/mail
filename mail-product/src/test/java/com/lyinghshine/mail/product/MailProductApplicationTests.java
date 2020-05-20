46ef9389de0d6d44f146cefe357bc7385660e551
lyingshine <189865728@qq.com>
2020-05-20 11:10:07 +0800

    mail 0.0.1

---
+++ mail-product/src/test/java/com/lyinghshine/mail/product/MailProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyinghshine.mail.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mail-ware/src/test/java/com/lyingshine/mail/ware/MailWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyingshine.mail.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
